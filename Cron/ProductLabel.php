81b89394d890e9b665f6c673f47b6d3e3ee916d1
Bohdan Berezhniy <bohdan.berezhniy@magefan.com>
2024-08-14 16:13:25 +0300

    modified rule_apply_type to keep it available in basic and plus version

--- Cron/ProductLabel.php
+++ Cron/ProductLabel.php
@@ -7,7 +7,10 @@ declare(strict_types=1);
 
 namespace Magefan\ProductLabel\Cron;
 
+use Magefan\ProductLabel\Model\Config;
+use Magefan\ProductLabel\Model\ProductLabelAction;
 use Magefan\ProductLabel\Model\Config\Source\ApplyByOptions;
+use Magefan\Community\Api\GetModuleVersionInterface;
 
 /**
  * Apply Product Label rules
@@ -15,26 +18,33 @@ use Magefan\ProductLabel\Model\Config\Source\ApplyByOptions;
 class ProductLabel
 {
     /**
-     * @var \Magefan\ProductLabel\Model\Config
+     * @var Config
      */
     protected $config;
 
     /**
-     * @var \Magefan\ProductLabel\Model\ProductLabelAction
+     * @var ProductLabelAction
      */
     protected $productLabelAction;
 
     /**
-     * ProductLabel constructor.
-     * @param \Magefan\ProductLabel\Model\ProductLabelAction $productLabelAction
-     * @param \Magefan\ProductLabel\Model\Config $config
+     * @var GetModuleVersionInterface
+     */
+    protected $getModuleVersion;
+
+    /**
+     * @param ProductLabelAction $productLabelAction
+     * @param Config $config
+     * @param GetModuleVersionInterface $getModuleVersion
      */
     public function __construct(
-        \Magefan\ProductLabel\Model\ProductLabelAction $productLabelAction,
-        \Magefan\ProductLabel\Model\Config $config
+        ProductLabelAction $productLabelAction,
+        Config $config,
+        GetModuleVersionInterface $getModuleVersion
     ) {
         $this->config = $config;
         $this->productLabelAction = $productLabelAction;
+        $this->getModuleVersion = $getModuleVersion;
     }
 
     /**
@@ -43,7 +53,14 @@ class ProductLabel
     public function execute(): void
     {
         if ($this->config->isEnabled()) {
-            $this->productLabelAction->execute(['rule_apply_type' => ApplyByOptions::CRON]);
+            // to allow execute cron in basic and plus versions
+            $rule_apply_type = ApplyByOptions::MANUALLY;
+
+            if ($this->getModuleVersion->execute('Magefan_ProductLabelExtra')) {
+                $rule_apply_type = ApplyByOptions::CRON;
+            }
+
+            $this->productLabelAction->execute(['rule_apply_type' => $rule_apply_type]);
         }
     }
 }
