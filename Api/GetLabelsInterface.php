7ee550fb909df80d1dfc2297119722fe6569b2ff
Yuriy <yuri@tandem-dev.de>
2024-12-19 15:48:57 +0200

    12453-graph-ql-support-2

--- Api/GetLabelsInterface.php
+++ Api/GetLabelsInterface.php
@@ -3,11 +3,10 @@
  * Copyright © Magefan (support@magefan.com). All rights reserved.
  * Please visit Magefan.com for license details (https://magefan.com/end-user-license-agreement).
  */
+declare(strict_types=1);
+
 namespace Magefan\ProductLabel\Api;
 
-/**
- * Interface UrlResolverInterface
- */
 interface GetLabelsInterface
 {
     /**
